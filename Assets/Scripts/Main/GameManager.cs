d7d463ed6124bb9bfc924b336c3c74297f1a3391
Deependra-Das <iamdeep75@gmail.com>
2026-09-09 16:25:24 +0530

    Updated GameManager to add references

--- Assets/Scripts/Main/GameManager.cs
+++ Assets/Scripts/Main/GameManager.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Projectile_SO _projectile_SO;
+    [SerializeField] private Transform _enemyProjectilePoolParent;
+    [SerializeField] private Transform _droneProjectilePoolParent;
+
     public static GameManager Instance { get; private set; }
     public ServiceLocator Services { get; private set; }
+
+    private ProjectilePoolService _projectilePoolService;
 
     private void Awake()
     {
@@ -21,14 +27,17 @@ public class GameManager : MonoBehaviour
     {
         InitializeServices();
         RegisterServices();
+        GameplayManager.Instance.Initialize(_projectilePoolService);
     }
 
     private void InitializeServices()
     {
         Services = new ServiceLocator();
+        _projectilePoolService = new ProjectilePoolService(_projectile_SO, _enemyProjectilePoolParent, _droneProjectilePoolParent);
     }
 
     private void RegisterServices()
     {
+        Services.Register(_projectilePoolService);
     }
 }
